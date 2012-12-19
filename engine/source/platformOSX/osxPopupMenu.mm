8e4cfd4f1bed9539be765bd5de918b4f794f2b9f
MichPerry-GG <michaelp@garagegames.com>
2012-12-19 11:40:59 -0500

    - Got the basis down for our OS X PopupMenu. Gonna need an intermediate object to handle clicks. Currently it's named osxPopupMenuController. This is a utility class that will react to clicks and call its owner (PopupMenu*) ->handleSelect.
    - Removed some OS X default menu items that are not goign to be used.

--- engine/source/platformOSX/osxPopupMenu.mm
+++ engine/source/platformOSX/osxPopupMenu.mm
@@ -4,29 +4,132 @@
 //-----------------------------------------------------------------------------
 #include "platformOSX/platformOSX.h"
 #include "platform/menus/popupMenu.h"
+#include "memory/safeDelete.h"
+#include "gui/guiCanvas.h"
+
+@interface osxPopupMenuController : NSObject
+{
+    NSMenu* _menu;
+    NSMenuItem* _menuItem;
+    PopupMenu* _owner;
+}
+
+@property NSMenu* menu;
+@property NSMenuItem* menuItem;
+@property PopupMenu* owner;
+
+-(void)handleSelect:(id)sender;
+
+@end
+
+@implementation osxPopupMenuController
+
+@synthesize menu = _menu;
+@synthesize menuItem = _menuItem;
+@synthesize owner = _owner;
+
+-(id) init
+{
+    self = [super init];
+
+    if (self)
+    {
+        _owner = NULL;
+        _menu = [[NSMenu allocWithZone:[NSMenu menuZone]] initWithTitle:@"New Menu"];
+        _menuItem = [[NSMenuItem allocWithZone:[NSMenu menuZone]] initWithTitle:@"New Menu Item" action:NULL keyEquivalent:@""];
+        [_menuItem setSubmenu:_menu];
+    }
+}
+
+-(void) dealloc
+{
+    if (_menu)
+        [_menu release];
+
+    if (_menuItem)
+        [_menuItem release];
+
+    [super dealloc];
+}
+
+-(void)handleSelect:(id)sender
+{
+    if(_owner)
+        _owner->handleSelect(3, [[_menu title] UTF8String]);
+}
+
+@end
+
+class PlatformPopupMenuData
+{
+public:
+
+    osxPopupMenuController* mController;
+    S32 tag;
+
+    PlatformPopupMenuData()
+    {
+        mController = NULL;
+
+        tag = getTag();
+    }
+
+    ~PlatformPopupMenuData()
+    {
+        if (mController)
+            [mController release];
+
+        mController = NULL;
+    }
+
+    // We assign each new menu item an arbitrary integer tag.
+    static S32 getTag()
+    {
+        static S32 lastTag = 'TORQ';
+        return ++lastTag;
+    }
+};
 
 //-----------------------------------------------------------------------------
-#pragma message ("PopupMenu::createPlatformPopupMenuData not yet implemented")
-void PopupMenu::createPlatformPopupMenuData()
+// Allocates the OS X specific PopupMenuData
+ void PopupMenu::createPlatformPopupMenuData()
 {
+    mData = new PlatformPopupMenuData;
 }
 
 //-----------------------------------------------------------------------------
-#pragma message ("PopupMenu::deletePlatformPopupMenuData not yet implemented")
+// Deallocates the OS X specific PopupMenuData
 void PopupMenu::deletePlatformPopupMenuData()
 {
+    SAFE_DELETE(mData);
 }
 
 //-----------------------------------------------------------------------------
-#pragma message ("PopupMenu::createPlatformMenu not yet implemented")
+// Called in PopupMenu's constructor, this handles platform specific menu setup
 void PopupMenu::createPlatformMenu()
 {
+    mData->mController = [[osxPopupMenuController alloc] init];
+    [mData->mController setOwner:this];
 }
 
 //-----------------------------------------------------------------------------
 #pragma message ("PopupMenu::insertItem not yet implemented")
-S32 PopupMenu::insertItem(S32 pos, const char *title, const char* accel)
+S32 PopupMenu::insertItem(S32 pos, const char *title, const char *accel)
 {
+    NSMenuItem *newItem;
+
+    newItem = [[NSMenuItem allocWithZone:[NSMenu menuZone]]
+            initWithTitle:[NSString stringWithUTF8String:title]
+            action:NULL
+            keyEquivalent:[NSString stringWithUTF8String:accel]];
+
+    [newItem setTarget:mData->mController];
+    [newItem setAction:@selector(handleSelect:)];
+
+    [[mData->mController menu] addItem:newItem];
+
+    [newItem release];
+
     return 0;
 }
 
@@ -79,23 +182,40 @@ bool PopupMenu::canHandleID(U32 iD)
 #pragma message ("PopupMenu::handleSelect not yet implemented")
 bool PopupMenu::handleSelect(U32 command, const char *text /* = NULL */)
 {
-    return false;
+    //return dAtob(Con::executef(this, 4, "onSelectItem", Con::getIntArg(pos - 1), text ? text : ""));
 }
 
 //-----------------------------------------------------------------------------
 #pragma message ("PopupMenu::showPopup not yet implemented")
 void PopupMenu::showPopup(S32 x /* = -1 */, S32 y /* = -1 */)
 {
+    // Get the position of the cursor
+    if(x < 0 || y < 0)
+    {
+        Point2I p = Canvas->getCursorPos();
+        x = p.x;
+        y = p.y;
+    }
+
+    // Convert to native coordinates
+    //CGPoint native = MacCarbTorqueToNativeCoords(x, y);
+
+    // Manually click the menu item
+    //U32 result = PopUpMenuSelect(mData->mMenu, native.y, native.x, 0);
 }
 
 //-----------------------------------------------------------------------------
 #pragma message ("PopupMenu::attachToMenuBar not yet implemented")
 void PopupMenu::attachToMenuBar(S32 pos, const char *title)
 {
+    [[mData->mController menuItem] setTitle:[NSString stringWithUTF8String:title]];
+    [[mData->mController menu] setTitle:[NSString stringWithUTF8String:title]];
+    [[NSApp mainMenu] addItem:[mData->mController menuItem]];
 }
 
 //-----------------------------------------------------------------------------
 #pragma message ("PopupMenu::removeFromMenuBar not yet implemented")
 void PopupMenu::removeFromMenuBar()
 {
+    [[NSApp mainMenu] removeItem:[mData->mController menuItem]];
 }
