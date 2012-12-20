cd0d7a3e881a971c43304e01bc8d20f5e25e96ba
MichPerry-GG <michaelp@garagegames.com>
2012-12-20 15:17:52 -0500

    - Added a new {TestingModule}. Modifying main.michtests.cs has become too tedious and I can't use 3SS to test my latest PopupMenu changes. This is a WIP module that will eventually contain all the test code from main.michtests.cs and {MelvTesting}.

---
+++ modules/{Testing}/1/main.cs
@@ -0,0 +1,46 @@
+//-----------------------------------------------------------------------------
+// Torque
+// Copyright GarageGames, LLC 2011
+//-----------------------------------------------------------------------------
+
+function startTesting(%scopeSet)
+{
+    //-----------------------------------------------------------------------------
+    // Execute scripts
+    //-----------------------------------------------------------------------------
+    exec("./scripts/setup.cs");
+    exec("./scripts/spriteTests.cs");
+    exec("./scripts/menuBuilder.cs");
+    
+    //-----------------------------------------------------------------------------
+    // Load GUIs
+    //----------------------------------------------------------------------------- 
+
+    //-----------------------------------------------------------------------------
+    // Initialization
+    //----------------------------------------------------------------------------- 
+
+    createDefaultProfile();
+    createDefaultCanvas();
+    createTestBindings();
+
+    runSpriteTests();
+    
+    //runDialogTests();
+    //runGUITests();
+    //runRandomTests();
+    
+    if (isObject(moveMap))
+        moveMap.push();
+}
+
+function endTesting()
+{
+    if (isObject(moveMap))
+    {
+        moveMap.pop();
+        moveMap.delete();
+    }
+    
+    endSpriteTests();
+}
---
+++ modules/{Testing}/1/scripts/setup.cs
@@ -0,0 +1,76 @@
+//-----------------------------------------------------------------------------
+// Torque
+// Copyright GarageGames, LLC 2011
+//-----------------------------------------------------------------------------
+
+function createDefaultProfile()
+{
+    if(!isObject(GuiDefaultProfile)) new GuiControlProfile (GuiDefaultProfile)
+    {
+        tab = false;
+        canKeyFocus = false;
+        hasBitmapArray = false;
+        mouseOverSelected = false;
+
+        // fill color
+        opaque = false;
+        fillColor = "211 211 211 64";
+        fillColorHL = "244 244 244 64";
+        fillColorNA = "244 244 244 64";
+        
+        // font
+        fontType = $platform $= "windows" ? "lucida console" : "monaco";
+        fontSize = 12;
+        fontColor = "255 255 255 255";
+
+        // border color
+        border = 1;
+        borderColor   = "100 100 100 255";
+        borderColorHL = "128 128 128";
+        borderColorNA = "64 64 64";
+
+    };
+}
+
+//-----------------------------------------------------------------------------
+
+function createDefaultCanvas()
+{
+    if (!createCanvas("Torque 2D"))
+    {
+        error("Canvas creation failed. Shutting down.");
+        quit();
+    }
+    
+    echo("- Created canvas successfully");
+    
+    if (!setScreenMode( 1024, 768, 32, false ))
+    {
+        error("Could not set screen mode");
+        quit();
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+function escapeQuit(%val)
+{
+    quit();
+}
+
+//-----------------------------------------------------------------------------
+
+function createTestBindings()
+{
+    new ActionMap(moveMap);
+    
+    moveMap.bind(keyboard, escape, "escapeQuit");
+}
+
+//-----------------------------------------------------------------------------
+
+function resetCanvas()
+{
+    if (isObject(Canvas))
+        Canvas.repaint();
+}
