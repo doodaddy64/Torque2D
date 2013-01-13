945324ecb4300ae57b11585ef5b723eedd1c6e6e
MelvMay-GG <melvm@garagegames.com>
2013-01-13 01:05:40 +0000

    - Sandbox WIP.

--- sandbox/modules/Sandbox/1/scripts/toolbox.cs
+++ sandbox/modules/Sandbox/1/scripts/toolbox.cs
@@ -26,13 +26,52 @@ function initializeToolbox()
         // Fetch stock color name.
         %colorName = getStockColorName(%i);
         
-        // Add to list.        
+        // Add to the list.        
         BackgroundColorSelectList.add( getStockColorName(%i), %i );
         
         // Select the color if it's the default one.
         if ( %colorName $= $defaultSceneColor )
             BackgroundColorSelectList.setSelected( %i );
     }
+    
+    // Fetch the active resolution.
+    %activeResolution = getRes();
+    %activeWidth = getWord(%activeResolution, 0);
+    %activeHeight = getWord(%activeResolution, 1);
+    %activeBpp = getWord(%activeResolution, 2);
+    
+    // Fetch the resolutions.
+    %resolutionList = getResolutionList( $pref::Video::displayDevice );
+    %resolutionCount = getWordCount( %resolutionList ) / 3;
+    %inputIndex = 0;
+    %outputIndex = 0;
+    for( %i = 0; %i < %resolutionCount; %i++ )
+    {
+        // Fetch the resolution entry.
+        %width = getWord( %resolutionList, %inputIndex );
+        %height = getWord( %resolutionList, %inputIndex+1 );
+        %bpp = getWord( %resolutionList, %inputIndex+2 );
+        %inputIndex += 3;
+        
+        // Skip the 16-bit ones.
+        if ( %bpp == 16 )
+            continue;
+            
+        // Store the resolution.
+        $sandboxResolutions[%outputIndex] = %width SPC %height SPC %bpp;
+        
+        // Add to the list.
+        ResolutionSelectList.add( %width @ "x" @ %height SPC "(" @ %bpp @ ")", %outputIndex );
+        
+        // Select the resolution if it's the default one.
+        if ( %width == %activeWidth && %height == %activeHeight && %bpp == %activeBpp )
+            ResolutionSelectList.setSelected( %outputIndex );
+            
+        %outputIndex++;
+    }
+    
+    // Set the fullscreen flag.
+    $sandboxFullscreen = $pref::Video::fullScreen;
 }
 
 //-----------------------------------------------------------------------------
@@ -97,6 +136,24 @@ function BackgroundColorSelectList::onSelect(%this)
 
 //-----------------------------------------------------------------------------
 
+function ResolutionSelectList::onSelect(%this)
+{
+    // Finish if the sandbox scene is not available.
+    if ( !isObject(SandboxScene) )
+        return;
+            
+    // Fetch the index.
+    %index = %this.getSelected();
+
+    // Fetch resolution.
+    %resolution = $sandboxResolutions[%index];
+    
+    // Set the screen mode.    
+    setScreenMode( GetWord( %resolution , 0 ), GetWord( %resolution, 1 ), GetWord( %resolution, 2 ), $sandboxFullscreen );
+}
+
+//-----------------------------------------------------------------------------
+
 function updateToolboxOptions()
 {
     // Finish if the sandbox scene is not available.
@@ -106,6 +163,10 @@ function updateToolboxOptions()
     // Set the scene color.
     SandboxScene.BackgroundColor = getStockColorName($activeSceneColor);
     SandboxScene.UseBackgroundColor = true;        
+    
+    // Set the full-screen mode appropriately.
+    if ( isFullScreen() != $sandboxFullscreen )
+        toggleFullScreen();
     
     // Set option.
     if ( $metricsOption )
