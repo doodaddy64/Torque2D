f0db28777b41f0808a2544ee9892500908f83422
MelvMay-GG <melvm@garagegames.com>
2013-01-14 11:12:46 +0000

    - Fixed build error due to carbon being removed.

--- engine/source/platform/platformGL.h
+++ engine/source/platform/platformGL.h
@@ -6,7 +6,7 @@
 #ifndef _PLATFORMGL_H_
 // redirect to appropriate platform file
 #if defined(TORQUE_OS_OSX)
-#include "platformMacCarb/platformGL.h"
+#include "platformOSX/platformGL.h"
 #elif defined(TORQUE_OS_WIN32)
 #include "platformWin32/platformGL.h"
 #elif defined(TORQUE_OS_LINUX) || defined(TORQUE_OS_OPENBSD) || defined(TORQUE_OS_FREEBSD)
