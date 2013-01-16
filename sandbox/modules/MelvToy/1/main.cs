d9e4b12dc4f0b5b37021338cefbde208261e4f98
MelvMay-GG <melvm@garagegames.com>
2013-01-16 13:17:29 +0000

    - Particles WIP.

--- sandbox/modules/MelvToy/1/main.cs
+++ sandbox/modules/MelvToy/1/main.cs
@@ -7,9 +7,11 @@ function createMelvToy( %scopeSet )
 {
     // Load scripts.
     exec( "./scripts/compositeSpriteTest.cs" );
+    exec( "./scripts/particleTest.cs" );
     
     // Run test.
-    runCompositeSpriteTest( %scopeSet );
+    //runCompositeSpriteTest();
+    runParticleTest();
 }
 
 //-----------------------------------------------------------------------------
