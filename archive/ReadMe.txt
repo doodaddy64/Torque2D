e51b7617bd0bd064f32a934b87ae10fa2402b27a
MelvMay-GG <melvm@garagegames.com>
2013-01-22 18:15:38 +0000

    - Removed a whole bunch of redundant stuff.

--- archive/ReadMe.txt
+++
@@ -1,2 +0,0 @@
-This is a temporary holding area for files we may need to used as a reference later.
-These files should NOT be used in a production release!
--- engine/source/editor/assetFunctions.h
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------------------------------
-// Torque
-// Copyright GarageGames, LLC 2011
-//-----------------------------------------------------------------------------
-
-#ifndef _ASSET_FUNCTIONS_H_
-#define _ASSET_FUNCTIONS_H_
-
-#endif // _ASSET_FUNCTIONS_H_
