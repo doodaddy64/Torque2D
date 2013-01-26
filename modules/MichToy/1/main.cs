7cccfa11c851842fbacb3fd09205cbc4d7652ea7
MelvMay-GG <melvm@garagegames.com>
2013-01-26 07:02:39 +0000

    - Added the ability to render the FPS in MichToy on iOS without fonts so we can test this slow-down bug.

--- modules/MichToy/1/main.cs
+++ modules/MichToy/1/main.cs
@@ -12,16 +12,39 @@ function createMichToy( %scopeSet )
     %sprite.Size = 60;
     %sprite.AngularVelocity = 90;
 
-    %imageFont = new ImageFont();
-    SandboxScene.add( %imageFont );
-    %imageFont.image = "Sandbox:font";
-    %imageFont.fontSize = 3;
-    %imageFont.text = "This is a test!";
+    $imageFont = new ImageFont();
+    SandboxScene.add( $imageFont );
+    $imageFont.image = "Sandbox:font";
+    $imageFont.fontSize = 3;
+    $imageFont.text = "This is a test!";
     
+    // Schedule to create a ball.
+    $updateFPSSchedule = schedule( 100, 0, updateMichToyFPS );   
+}
+
+//-----------------------------------------------------------------------------
+
+function updateMichToyFPS()
+{
+    // Reset the event schedule.
+    $createTumblerBallSchedule = "";
+    
+    $imageFont.text = $fps::framePeriod;
+    
+    // Schedule to create a ball.
+    $createTumblerBallSchedule = schedule( 100, 0, updateMichToyFPS );    
 }
 
 //-----------------------------------------------------------------------------
 
 function destroyMichToy( %scopeSet )
-{
+{   
+    // Cancel any pending events.
+    if ( isEventPending($createTumblerBallSchedule) )
+    {
+        cancel($createTumblerBallSchedule);
+        $createTumblerBallSchedule = "";
+    }    
+    
+    $imageFont = "";    
 }
