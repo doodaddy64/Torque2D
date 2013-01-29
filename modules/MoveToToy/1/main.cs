e84fafe3afa7fd899a502f8797565dd550f422c6
MelvMay-GG <melvm@garagegames.com>
2013-01-29 19:59:53 +0000

    - Added a "MoveTo" feature toy.  All seems to be working to me.

---
+++ modules/MoveToToy/1/main.cs
@@ -0,0 +1,68 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2013 GarageGames, LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//-----------------------------------------------------------------------------
+
+function createMoveToToy( %scopeSet )
+{
+    // Set the sandbox drag mode availability.
+    setSandboxDragModeAvailable( "pan", false );
+    setSandboxDragModeAvailable( "zoom", false );
+    
+    // Set drag mode to off.
+    setNextDragMode( "off" );
+
+    // Create the test sprite.
+    createMoveToSprite();
+    
+    // Activate the package.
+    activatePackage( MoveToToyPackage );    
+}
+
+//-----------------------------------------------------------------------------
+
+function createMoveToSprite()
+{
+    $moveToSprite = new Sprite();
+    $moveToSprite.Image = "ToyAssets:Crosshair2";
+    $moveToSprite.Size = 20;
+    SandboxScene.add( $moveToSprite );
+}
+
+//-----------------------------------------------------------------------------
+
+function destroyMoveToToy( %scopeSet )
+{
+    // Deactivate the package.
+    deactivatePackage( MoveToToyPackage );
+}
+
+//-----------------------------------------------------------------------------
+
+package MoveToToyPackage
+{
+
+function SandboxWindow::onTouchDown(%this, %touchID, %worldPos)
+{
+    // Move to the touched position.
+    $moveToSprite.moveTo( %worldPos );
+}
+    
+};
