50f0552222b4076df32764c76e49802120cdc5ab
MelvMay-GG <melvm@garagegames.com>
2013-01-30 16:09:22 +0000

    - Simple" ImageFontToy" WIP.

---
+++ modules/SimpleImageFontToy/1/main.cs
@@ -0,0 +1,67 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2013 GarageGames, LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//-----------------------------------------------------------------------------
+
+function createSimpleImageFontToy( %scopeSet )
+{
+    // Reset the toy.    
+    SimpleImageFontToy.reset();
+}
+
+//-----------------------------------------------------------------------------
+
+function SimpleImageFontToy::reset( %this )
+{    
+    // Create the image font.
+    %object = new ImageFont();
+    
+    // Always try to configure a scene-object prior to adding it to a scene for best performance.
+    
+    // Set the image font to use the font image asset.
+    %object.Image = "ToyAssets:Font";
+    
+    // We don't really need to do this as the position is set to zero by default.
+    %object.Position = "0 0";
+    
+    // We don't need to size this object as it sizes automatically according to the alignment, font-size and text.
+   
+    // Set the font size in both axis.  This is in world-units and not typicaly font "points".
+    %object.FontSize = "10 20";
+    
+    // We don't really need to do this as the padding is set to zero by default.
+    // Padding is specified in world-units and relates to the space added between each character.
+    %object.FontPadding = 0;
+
+    // Set the text alignment.
+    %object.TextAlignment = "Center";
+
+    // Set the text to display.
+    %object.Text = "T2D Sandbox";
+    
+    // Add the sprite to the scene.
+    SandboxScene.add( %object );    
+}
+
+//-----------------------------------------------------------------------------
+
+function destroySimpleSpriteToy( %scopeSet )
+{
+}
