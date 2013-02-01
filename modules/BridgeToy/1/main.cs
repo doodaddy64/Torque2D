a668b42f7e49c1f88a83ea0b036d3ab40f101493
Michael Perry <michaelp@garagegames.com>
2013-02-01 10:56:40 -0500

    - First pass at BridgeToy. Still want to add some objects to fall on the bridge and change some of the art.

--- modules/BridgeToy/1/main.cs
+++ modules/BridgeToy/1/main.cs
@@ -20,14 +20,127 @@
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
 
-function BridgeToy::create( %this )
+function BridgeToy::create(%this)
 {
-      
+    // Set the sandbox drag mode availability.
+    setSandboxDragModeAvailable("pan", true);
+    setSandboxDragModeAvailable("pull", true);
+    
+    // Set the drag mode.
+    setSandboxDragMode("pull");
+    
+    BridgeToy.GroundWidth = 150;
+    
+    // Reset the toy initially.
+    BridgeToy.reset();
 }
 
 //-----------------------------------------------------------------------------
 
-function BridgeToy::destroy( %this )
+function BridgeToy::destroy(%this)
 {   
        
+}
+
+//-----------------------------------------------------------------------------
+
+function BridgeToy::reset(%this)
+{
+    // Clear the scene.
+    SandboxScene.clear();
+    
+    // Zoom the camera in    
+    SandboxWindow.setCurrentCameraZoom(2);
+    
+    // Prefer the collision option off as it severely affects the performance.
+    setCollisionOption(false);
+    
+    // Set the scene gravity.
+    SandboxScene.setGravity(0, -20);
+    
+    // Set the drag mode as "pull".
+    setSandboxDragMode("pull");
+    
+    %ground = new Scroller();
+    %ground.setBodyType("static");
+    %ground.Image = "ToyAssets:woodGround";
+    %ground.setSize(BridgeToy.GroundWidth, 2);
+    %ground.setRepeatX(BridgeToy.GroundWidth / 12);   
+    %ground.setPosition(0, -10);
+    %ground.createEdgeCollisionShape(BridgeToy.GroundWidth/-2, 1, BridgeToy.GroundWidth/2, 1);
+    SandboxScene.add(%ground);
+    
+    %this.createBase(-12, -6.5, -90);
+    %this.createBase(11.5, -6.5, -90);
+    
+    %this.createBridge(-11.5, -4.1, 45);
+}
+
+//-----------------------------------------------------------------------------
+
+function BridgeToy::createBase(%this, %posX, %posY, %angle)
+{
+    %image = "ToyAssets:woodGround";
+
+    %obj = new Sprite();   
+    %obj.setBodyType("static");
+    %obj.setImage("ToyAssets:woodGround");
+    %obj.setSize(5, 0.8);   
+    %obj.createPolygonBoxCollisionShape(5, 0.8);
+    %obj.setPosition(%posX, %posY);
+    %obj.setAngle(%angle);    
+    %obj.setAwake(false);
+    %obj.setDefaultFriction(1.0);
+    
+    SandboxScene.add(%obj);
+}
+
+function BridgeToy::createBridge(%this, %posX, %posY, %linkCount)
+{
+    %linkWidth = 0.5;
+    %linkHeight = %linkWidth * 0.5;
+    %halfLinkWidth = %linkWidth * 0.5;
+
+    %rootObj = new Sprite();
+    %rootObj.setBodyType("static");
+    %rootObj.setImage("ToyAssets:cable");
+    %rootObj.setPosition(%posX, %posY);
+    %rootObj.setSize(%linkWidth, %linkHeight);
+    %rootObj.setCollisionSuppress();
+    SandboxScene.add(%rootObj);
+
+    %lastLinkObj = %rootObj;
+
+    for (%n = 1; %n <= %linkCount; %n++)
+    {
+        %obj = new Sprite();
+
+        %obj.setImage("ToyAssets:cable");
+        %obj.setPosition(%posX + (%n*%linkWidth), %posY);
+        %obj.setSize(%linkWidth, %linkHeight);
+        
+        if (%n == %linkCount)
+        {
+            %obj.setBodyType("static");
+            %obj.setCollisionSuppress();
+        }
+        else
+        {      
+            %obj.setDefaultDensity(1);
+            %obj.setDefaultFriction(1.0);
+            %obj.createPolygonBoxCollisionShape(%linkWidth, %linkHeight);
+            %obj.setAngularDamping(1.0);
+            %obj.setLinearDamping(1.0);
+        }
+        
+        SandboxScene.add(%obj);   
+
+        SandboxScene.createRevoluteJoint(%lastLinkObj, %obj, %halfLinkWidth, 0, -%halfLinkWidth, 0);
+        %joint = SandboxScene.createMotorJoint(%lastLinkObj, %obj);
+        SandboxScene.setMotorJointMaxForce(%joint, 500);
+        %obj.setAwake(false);
+        %lastLinkObj.setAwake(false);
+        
+        %lastLinkObj = %obj;
+    }
 }
