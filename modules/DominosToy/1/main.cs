d7cf4f252fa83cc9311fb50cb3a8885bae30f03b
Mich <michaelp@garagegames.com>
2013-02-01 15:50:48 -0500

    - Dominos toy WIP

--- modules/DominosToy/1/main.cs
+++ modules/DominosToy/1/main.cs
@@ -22,7 +22,18 @@
 
 function DominosToy::create( %this )
 {
-      
+    // Set the sandbox drag mode availability.
+    Sandbox.allowManipulation( pan );
+    Sandbox.allowManipulation( pull );
+
+    // Set the manipulation mode.
+    Sandbox.useManipulation( pull );
+
+    // Sets how large the ground will be
+    DominosToy.GroundWidth = 80;
+
+    // Reset the toy initially.
+    DominosToy.reset();
 }
 
 //-----------------------------------------------------------------------------
@@ -30,4 +41,122 @@ function DominosToy::create( %this )
 function DominosToy::destroy( %this )
 {   
        
+}
+
+//-----------------------------------------------------------------------------
+
+function DominosToy::reset(%this)
+{
+    // Clear the scene.
+    SandboxScene.clear();
+
+    // Zoom the camera in
+    SandboxWindow.setCurrentCameraArea("-10 0 15 15");
+
+    // Prefer the collision option off as it severely affects the performance.
+    setCollisionOption(false);
+
+    // Set the scene gravity.
+    SandboxScene.setGravity(0, -9.8);
+
+    // Create the ground
+    %ground = new Scroller();
+    %ground.setBodyType("static");
+    %ground.Image = "ToyAssets:woodGround";
+    %ground.setSize(DominosToy.GroundWidth, 2);
+    %ground.setRepeatX(DominosToy.GroundWidth / 12);
+    %ground.setPosition(0, 0);
+    %ground.createEdgeCollisionShape(DominosToy.GroundWidth/-2, 1, DominosToy.GroundWidth/2, 1);
+    SandboxScene.add(%ground);
+
+    // Create the static fixtures the dominos collide against
+    %this.createPlatforms();
+
+    // Create the dominos
+    %this.createDominos();
+
+    // Create the lever platform
+    %this.createLever();
+
+    // Create the trap at the end of the gadget
+    %this.createTrap();
+
+    // Create the swinging box that starts the dominos
+    %this.createSwinger();
+}
+
+//-----------------------------------------------------------------------------
+
+function DominosToy::createPlatforms()
+{
+    // Create top platform
+    %obj = new Sprite();
+    %obj.setBodyType("static");
+    %obj.Image = "ToyAssets:woodGround";
+    %obj.setSize(4, 0.25);
+    %obj.setPosition(-0.5, 5);
+    %obj.createPolygonBoxCollisionShape( 4, 0.25 );
+    SandboxScene.add(%obj);
+
+    // Create slanted platform
+    %obj = new Sprite();
+    %obj.setBodyType("static");
+    %obj.Image = "ToyAssets:woodGround";
+    %obj.setSize(7, 0.25);
+    %obj.setPosition(1, 3.5);
+    %obj.setAngle(15);
+    %obj.createPolygonBoxCollisionShape( 7, 0.25 );
+
+    SandboxScene.add(%obj);
+
+    // Create vertical platform
+    %obj = new Sprite();
+    %obj.setBodyType("static");
+    %obj.Image = "ToyAssets:woodGround";
+    %obj.setSize(1, 0.25);
+    %obj.setPosition(-3.25, 2.5);
+    %obj.setAngle(90);
+    %obj.createPolygonBoxCollisionShape( 1, 0.25 );
+
+    SandboxScene.add(%obj);
+}
+
+//-----------------------------------------------------------------------------
+
+function DominosToy::createDominos()
+{
+    for (%i = 0; %i < 10; %i++)
+    {
+        %obj = new Sprite();
+
+        %obj.Image = "ToyAssets:woodGround";
+        %obj.setSize(0.1, 0.5);
+        %obj.setDefaultFriction(0.1);
+        %xPos = (0.35 * %i) - 2;
+        %obj.setPosition(%xPos, 5.75);
+
+        %obj.createPolygonBoxCollisionShape( 0.1, 0.5 );
+        SandboxScene.add(%obj);
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+function DominosToy::createLever()
+{
+
+}
+
+//-----------------------------------------------------------------------------
+
+function DominosToy::createTrap()
+{
+
+}
+
+//-----------------------------------------------------------------------------
+
+function DominosToy::createSwinger()
+{
+
 }
