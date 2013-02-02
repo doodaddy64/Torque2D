70668e2449b51a37ec36280dfd22c9dcf2769d2e
MelvMay-GG <melvm@torquepowered.com>
2013-02-02 09:47:31 +0000

    - Set a range of scene-layers in the Aquarium Toy to use "batch" mode so the fish are sorted into an order for reducing draw-calls rather than their age.

--- modules/AquariumToy/1/main.cs
+++ modules/AquariumToy/1/main.cs
@@ -30,10 +30,15 @@ function AquariumToy::create( %this )
     AquariumToy.currentFish = 0;
     AquariumToy.selectedAnimation = "AquariumToy:angelfish1Anim";
     
-    // All the fish are on a specific scene-layer so set this layer to sort in "batch" mode
+    // Set all the fish scene-layers to sort in "batch" mode
     // so that all the fish will be sorted into a batchable order to reduce draw calls.
+    SandboxScene.setLayerSortMode( 0, batch );
+    SandboxScene.setLayerSortMode( 1, batch );
     SandboxScene.setLayerSortMode( 2, batch );
-
+    SandboxScene.setLayerSortMode( 3, batch );
+    SandboxScene.setLayerSortMode( 4, batch );
+    SandboxScene.setLayerSortMode( 5, batch );
+    
     addNumericOption("Max Fish", 0, 50, 1, "setMaxFish", %this.maxFish, true);
     addSelectionOption(getFishAnimationList(), "Fish Animation", "setSelectedAnimation", false);
     addButtonOption("Spawn fish", "spawnOneFish", false);  
