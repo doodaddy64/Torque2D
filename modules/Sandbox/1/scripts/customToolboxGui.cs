5079be5de67da3d481158ae13685effd64649a42
Mich <michaelp@garagegames.com>
2013-02-03 07:49:30 -0500

    - Clamping values for numeric option to its min and max fields.

--- modules/Sandbox/1/scripts/customToolboxGui.cs
+++ modules/Sandbox/1/scripts/customToolboxGui.cs
@@ -372,7 +372,12 @@ function TextEditController::updateToy(%this)
 {
     if (%this.toy $= "")
         return;
-        
+    
+    if (%this.getText() > %this.max)
+        %this.setText(%this.max);
+    else if (%this.getText() < %this.min)
+        %this.setText(%his.min);
+    
     if (%this.callback !$= "" && %this.getValue() !$= "")
     {
         %setter = "%this.toy." @ %this.callback @ "(" @ %this.getValue() @ ");";
